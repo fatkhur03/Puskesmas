c12acafc1e03b31f2c7379ae125a017d6df57827
fatkhur <muhammadthursina123@gmail.com>
2023-05-19 14:33:51 +0700

    Tugas Pertemuan 9

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\PasienController;
+use App\Http\Controllers\DokterController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -26,3 +27,16 @@ Route::get('/pasien/create', [PasienController::class, 'create']);
 
 //root untuk memproses tambah pasien
 Route::post('/pasien', [PasienController::class, 'store']);
+
+
+
+
+
+//untuk menampilkan data Dokter
+Route::get('/dokter', [DokterController::class, 'index']);
+
+//untuk menampilkan Form tambah
+Route::get('/dokter/create', [DokterController::class, 'create']);
+
+//root untuk memproses tambah dokter
+Route::post('/dokter', [DokterController::class, 'store']);
