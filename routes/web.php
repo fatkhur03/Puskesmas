7123b4c05a69a11e8641427c337258200ef39c9d
fatkhur03 <muhammadthursina123@gmail.com>
2023-06-14 15:21:27 +0700

    Praktikum 13

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\PasienController;
 use App\Http\Controllers\DokterController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,46 +19,49 @@ use Illuminate\Support\Facades\Route;
 
 
 //untuk menampilkan data pasien
-Route::get('/pasien', [PasienController::class, 'index']);
+Route::get('/pasien', [PasienController::class, 'index'])->middleware('auth');
 
 //untuk menampilkan Form tambah
-Route::get('/pasien/create', [PasienController::class, 'create']);
+Route::get('/pasien/create', [PasienController::class, 'create'])->middleware('auth');
 
 //root untuk memproses tambah pasien
-Route::post('/pasien', [PasienController::class, 'store']);
+Route::post('/pasien', [PasienController::class, 'store'])->middleware('auth');
 
 
 
 
 
 //untuk menampilkan data Dokter
-Route::get('/dokter', [DokterController::class, 'index']);
+Route::get('/dokter', [DokterController::class, 'index'])->middleware('auth');
 
 //untuk menampilkan Form tambah
-Route::get('/dokter/create', [DokterController::class, 'create']);
+Route::get('/dokter/create', [DokterController::class, 'create'])->middleware('auth');
 
 //root untuk memproses tambah dokter
-Route::post('/dokter', [DokterController::class, 'store']);
+Route::post('/dokter', [DokterController::class, 'store'])->middleware('auth');
 
 //Route untuk menghapus data pasine
-Route::delete('/pasien', [PasienController::class, 'destroy']);
+Route::delete('/pasien', [PasienController::class, 'destroy'])->middleware('auth');
 
 //Route untuk menampilkan dashboard
-Route::get('/', [DashboardController::class, 'index']);
+Route::get('/', [DashboardController::class, 'index'])->middleware('auth');
 
 
 
 //Route untuk menampilkan form edit pasien
-Route::get('/pasien/edit/{id}', [PasienController::class, 'edit']);
+Route::get('/pasien/edit/{id}', [PasienController::class, 'edit'])->middleware('auth');
 
 //Route untukuntuk memproses form edit pasien
-Route::put('/pasien/{id}', [PasienController::class, 'update']);
+Route::put('/pasien/{id}', [PasienController::class, 'update'])->middleware('auth');
 
 
 
 //Route untuk menampilkan form edit Dokter
-Route::get('/dokter/edit/{id}', [DokterController::class, 'edit']);
+Route::get('/dokter/edit/{id}', [DokterController::class, 'edit'])->middleware('auth');
 
 //Route untukuntuk memproses form edit Dokter
-Route::put('/dokter/{id}', [DokterController::class, 'update']);
+Route::put('/dokter/{id}', [DokterController::class, 'update'])->middleware('auth');
 
+
+
+Auth::routes();
